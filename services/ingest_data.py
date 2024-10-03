201906e7d6655d4355aefb9c064c319cb7a85252
Mohamed Kaouech <123780902+KaouechMED@users.noreply.github.com>
2024-10-03 13:33:10 +0100

    elasticsearch client and images store added

---
+++ services/ingest_data.py
@@ -0,0 +1,55 @@
+import os
+from elasticsearch import Elasticsearch
+from vgg16_encoder import FeatureExtractor
+from PIL import Image
+
+
+# Function to connect to Elasticsearch and create an index
+def create_elastic_index(client, index_name):
+    if client.indices.exists(index=index_name):
+        print(f"Index {index_name} already exists. Skipping creation.")
+    else:
+        client.indices.create(index=index_name, body={
+            "mappings": {
+                "properties": {
+                    "path": {"type": "keyword"},
+                    "feature_vector": {"type": "dense_vector", "dims": 4096}
+                }
+            }
+        })
+        print(f"Index {index_name} created.")
+        
+
+# Function to index image data into Elasticsearch
+def index_image_data(client, index_name, image_path, feature_vector):
+    doc = {
+        "path": image_path,
+        "feature_vector": feature_vector.tolist()  
+    }
+    client.index(index=index_name, body=doc)
+    print(f"Indexed {image_path}")
+    
+
+if __name__ == "__main__":
+    
+    #elasticsearch connection
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+    # Create Elasticsearch index
+    index="image_features"
+    create_elastic_index(es,index)
+    #extrat features 
+    extractor=FeatureExtractor()
+    images_path="projetImageSearch\images"
+    #loop through images and extract features
+    for img_file in os.listdir(images_path):
+        
+        img_path = os.path.join(images_path, img_file)
+
+        # Load image using PIL
+        img = Image.open(img_path)
+
+        # Extract feature vector
+        feature_vector = extractor.extract(img)
+
+        # Index image path and feature vector into Elasticsearch
+        index_image_data(es, index, img_path, feature_vector)
--- services/main.py
+++ services/main.py
@@ -4,10 +4,15 @@ from flask_cors import CORS
 import io
 import base64
 from PIL import Image
+from elasticsearch import Elasticsearch
 
 app=Flask(__name__)
 CORS(app)
 extractor=FeatureExtractor()
+#elasticsearch client and index
+es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+index="image_features"
+similarity_threshold = 0.7
 
 @app.route('/api',methods=['GET'])
 def home():
@@ -19,8 +24,29 @@ def search():
     image_bytes = io.BytesIO(base64.b64decode(image))
     img=Image.open(image_bytes)
     embedding=extractor.extract(img).tolist()
-    return jsonify({'image_embeddings':embedding})
-    
+    search_query = {
+        "size": 10,  
+        "min_score": similarity_threshold + 1.0,  
+        "_source": ["path"],
+        "query": {
+            "script_score": {
+                "query": {"match_all": {}},
+                "script": {
+                    "source": """
+                        cosineSimilarity(params.query_vector, 'image_vector') + 1.0
+                    """,
+                    "params": {
+                        "query_vector": embedding
+                    }
+                }
+            }
+        }}
+    search_response = es.search(index=index, body=search_query)
+    if search_response['hits']['hits']:
+        paths = [hit["_source"]["path"] for hit in search_response['hits']['hits']]
+        return jsonify({'image_paths': paths})
+    else:
+        jsonify({'image_paths':[]})    
     
 
 if __name__ =="__main__":
